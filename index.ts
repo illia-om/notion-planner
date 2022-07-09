db6ac0ce5c791f66bf7a8b88c5006908eac3b94a
illia-om <illia.omelchenko@icloud.com>
2022-07-09 15:57:24 +0100

    add test data env

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send(`Express + TypeScript Server ${process.env.TEST_DATA}`);
 });
 
 app.listen(port, () => {
@@ -39,5 +39,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Received your message =)');
 });
