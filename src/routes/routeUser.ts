7b34532c826b2a83642f50a3ac8e0f78b2fc36c7
illia-om <illia.omelchenko@icloud.com>
2022-07-12 00:36:40 +0100

    hot fix

--- src/routes/routeUser.ts
+++ src/routes/routeUser.ts
@@ -4,7 +4,7 @@ export const routeUser: TAppRouter = (context) => {
     return (req, res) => {
         res.json({
             success: true,
-            user: req.user
+            user: 'req.user'
         });
     }
 };
