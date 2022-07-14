631f6edba93c4debf87e6a6a83343eb963e7141e
illia-om <illia.omelchenko@icloud.com>
2022-07-14 22:45:12 +0100

    add INotionIntegration type

--- src/types.ts
+++ src/types.ts
@@ -4,12 +4,23 @@ import TelegramBot from 'node-telegram-bot-api';
 import { Db } from './db';
 
 export interface IRouteContext {
-  readonly env: Record<string, string>
-  readonly telegramBot: TelegramBot;
-  readonly notion: Client;
-  readonly db: Db;
+	readonly env: Record<string, string>
+	readonly telegramBot: TelegramBot;
+	readonly notion: Client;
+	readonly db: Db;
 }
 
 export type TAppRouter = (
-  routeContext: IRouteContext,
+	routeContext: IRouteContext,
 ) => Router | ((req: Request, res: Response, next: NextFunction) => void);
+
+export interface INotionIntegration {
+	readonly botId: string;
+	readonly accessToken: string;
+	readonly workspaceId: string;
+	readonly owner: Record<string, string>;
+	readonly workspaceName: string;
+	readonly workspaceIcon: string;
+	readonly tokenType: string;
+	readonly dateCreated: Date;
+}
