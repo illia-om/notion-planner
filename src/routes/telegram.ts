997924388265222f0120c874dae0c5f9017c1720
illia-om <illia.omelchenko@icloud.com>
2022-07-16 13:51:55 +0100

    fix

--- src/routes/telegram.ts
+++ src/routes/telegram.ts
@@ -4,19 +4,20 @@ import { Router } from 'express';
 export const telegramRoute: TAppRouter = (context) => {
     const router = Router();
     return router
-        .post('/togglePulling', (req, res) => {
-            if (context.telegramBot.isPolling()) {
-                context.telegramBot.stopPolling();
-                res.json({
-                    success: true,
-                    telegramBotPullingStatus: 'pulling'
-                });
-            } else {
-                context.telegramBot.startPolling({ restart: true });
-                res.json({
-                    success: true,
-                    telegramBotPullingStatus: 'not-pulling'
-                });
-            }
-        })
+    // return router
+    //     .post('/togglePulling', (req, res) => {
+    //         if (context.telegramBot.isPolling()) {
+    //             context.telegramBot.stopPolling();
+    //             res.json({
+    //                 success: true,
+    //                 telegramBotPullingStatus: 'pulling'
+    //             });
+    //         } else {
+    //             context.telegramBot.startPolling({ restart: true });
+    //             res.json({
+    //                 success: true,
+    //                 telegramBotPullingStatus: 'not-pulling'
+    //             });
+    //         }
+    //     })
 };
