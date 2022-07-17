4ab79d4dfa913a7fc263997152345f2838eb8488
illia-om <illia.omelchenko@icloud.com>
2022-07-17 22:30:05 +0100

    make hendleMessage telegramm to add test notion item

--- src/telegram-bot/handleMessage.ts
+++ src/telegram-bot/handleMessage.ts
@@ -1,9 +1,36 @@
 import { TTelegramRouter } from './../types';
+import { Client } from '@notionhq/client';
 
-export const hendleMessage: TTelegramRouter = (context, { msg }) => {
-    console.log('hendleMessage: ', msg);
-    if (msg.entities && msg.entities[0].type === 'bot_command') {
-        return;
+export const hendleMessage: TTelegramRouter = async (context, { msg }) => {
+    try {
+        console.log('hendleMessage: ', msg);
+        if (msg.entities && msg.entities[0].type === 'bot_command') {
+            return;
+        }
+        const { rows: notionIntegration } = await context.db.getNotionIntegrationByTelegramUserId(String(msg.chat.id));
+        const token = notionIntegration[0].access_token;
+        const notion = new Client({
+            auth: token
+        });
+        const res = await notion.pages.create({
+              "parent": {
+                "type": "database_id",
+                "database_id": process.env.PLANNER_ID!
+              },
+              "properties": {
+                "title": {
+                  "title": [
+                    {
+                      "text": {
+                        "content": msg.text!
+                      }
+                    }
+                  ]
+                }
+              }
+            })
+        context.telegramBot.sendMessage(msg.chat.id, msg.text!);
+    } catch (err) {
+        console.log('hendleMessage ERROR: ', err);
     }
-    context.telegramBot.sendMessage(msg.chat.id, msg.text!);
 }
