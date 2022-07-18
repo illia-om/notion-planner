5f832c96e62a8be631498388870938d8466b75b5
Illia Omelchenko <illia.omelchenko@icloud.com>
2022-07-18 16:34:47 +0100

    fix typo

--- src/telegram-bot/handleMessage.ts
+++ src/telegram-bot/handleMessage.ts
@@ -29,7 +29,7 @@ export const hendleMessage: TTelegramRouter = async (context, { msg }) => {
                 }
               }
             })
-        context.telegramBot.sendMessage(msg.chat.id, 'Added new item to the inbox:\n' + sg.text!);
+        context.telegramBot.sendMessage(msg.chat.id, 'Added new item to the inbox:\n' + msg.text!);
     } catch (err) {
         console.log('hendleMessage ERROR: ', err);
     }
