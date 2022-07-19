a2239a45f5fa80014a0e259e930b0b9049a8279a
illia-om <illia.omelchenko@icloud.com>
2022-07-19 15:03:19 +0100

    add async capabilities

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -9,27 +9,32 @@ export interface IUser {
 }
 
 export const authentication: TAppRouter = (context) => {
-    return (req, res, next) => {
-        const authHeader = req.headers.authorization;
-        const state = req.query.state;
-        let token = '';
-
-        if (authHeader) {
-            token = authHeader.split(' ')[1];
-        } else if (state) {
-            token = String(state);
-        } else {
-            return res.sendStatus(401);
-        }
-
-        jwt.verify(token, context.env.SERVER_ACCESS_TOKEN_SECRET!, async (err, data) => {
-            if (err) {
-                return res.sendStatus(403);
+    return async (req, res, next) => {
+        try {
+            const authHeader = req.headers.authorization;
+            const state = req.query.state;
+            let token = '';
+            
+            if (authHeader) {
+                token = authHeader.split(' ')[1];
+            } else if (state) {
+                token = String(state);
+            } else {
+                return res.sendStatus(401);
             }
-            const userData = jwt.decode(token) as jwt.JwtPayload;
-            const userId = userData.username;
-            req.userId = userId;
-            next();
-        });
+            
+            jwt.verify(token, context.env.SERVER_ACCESS_TOKEN_SECRET!, async (err, data) => {
+                if (err) {
+                    return res.sendStatus(403);
+                }
+                const userData = jwt.decode(token) as jwt.JwtPayload;
+                const userId = userData.username;
+                req.userId = userId;
+                next();
+            });
+        } catch (err) {
+            console.log('authentication ERROR: ', err);
+            res.status(500).json({ message: 'authentication Failed' });
+        }
     }
 };
