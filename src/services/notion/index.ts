f8beffe9f53cdc32d6113fb261697ede41e1a716
illia-om <illia.omelchenko@icloud.com>
2022-07-19 15:03:05 +0100

    getPlannerItemTypes updatePlannerItemTypes notion Service

--- src/services/notion/index.ts
+++ src/services/notion/index.ts
@@ -1,6 +1,7 @@
 import { Client } from '@notionhq/client';
 import { Db } from './../../db';
 import { QueryResult } from 'pg';
+import { INotionPlannerItemTypesProperty } from './../../types';
 
 export class Notion {
     private readonly client: Client;
@@ -21,6 +22,31 @@ export class Notion {
         return this.client.search({});
     }
 
+    getDatabase(databaseId: string) {
+        return this.client.databases.retrieve({
+            database_id: databaseId
+        })
+    }
+
+    async getPlannerItemTypes(plannerDatabaseId: string) {
+        const planner = await this.getDatabase(plannerDatabaseId);
+        const parsePropertyName = (propertyName: string): string => propertyName.toLocaleLowerCase().trim();
+
+        const properties = Object.keys(planner.properties).map(propertyName => ({ id: propertyName, parsedName: parsePropertyName(propertyName) }));
+        const typePropertyId = properties.find(property => property.parsedName === 'type');
+        if (!typePropertyId) {
+            return undefined;
+        }
+        const typeProperty = planner.properties[typePropertyId.id];
+        if (typeProperty.type !== 'select') {
+            return undefined;
+        }
+        return {
+            id: typeProperty.id,
+            values: typeProperty.select.options
+        };
+    }
+
     addItemToDatabase(databaseId: string, itemProperties: any) {
         return this.client.pages.create({
             parent: {
@@ -33,5 +59,9 @@ export class Notion {
 
     updatePlannerDatabaseId(userId: string, databaseId: string): Promise<QueryResult> {
         return this.db.updateNotionIntegrationPlannerDb(userId, databaseId);
+    }
+
+    updatePlannerItemTypes(userId: string, itemTypes: INotionPlannerItemTypesProperty): Promise<QueryResult> {
+        return this.db.updateNotionIntegrationPlannerItemTypes(userId, itemTypes);
     }
 }
